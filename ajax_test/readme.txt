8ccfdf2253c270c8adc242adcd9f94733acd40b5
ghdwpaks <hjm7904@gmail.com>
2022-01-30 10:49:37 +0900

    spring_first_leg 에서 RequestMapping 학습중

---
+++ ajax_test/readme.txt
@@ -0,0 +1,12 @@
+https://coding-factory.tistory.com/144?category=785989
+
+
+https://roqkffhwk.tistory.com/1
+https://jquery.com/
+https://araikuma.tistory.com/640
+https://doolyit.tistory.com/20
+
+
+
+
+
--- spring_first_leg/src/main/java/com/ghd/myapp/HomeController.java
+++ spring_first_leg/src/main/java/com/ghd/myapp/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.bind.annotation.GetMapping;
 /**
  * Handles requests for the application home page.
  */
@@ -34,6 +35,23 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+
+	@RequestMapping(value = "/ops", method = RequestMethod.GET)
+	public String ops(Locale locale, Model model) {
+		System.out.println("ops 에 접속을 시도하셨습니다.");
+		
+		
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "/ops/ops_main";
 	}
 	
 }
